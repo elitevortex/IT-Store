3673196291f5d68fce75f1b8385690aa43985ed2
eddie.chan <echa0081@student.monash.edu>
2023-03-12 14:51:31 +1100

    Added console I/O Added MenuInput class

--- src/Store.java
+++ src/Store.java
@@ -1,23 +1,24 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Store {
-    private ArrayList<Computer> computers;
-    private ArrayList<Printer> printers;
+    private ArrayList<Computer> computers = new ArrayList<>();
+    private ArrayList<Printer> printers = new ArrayList<>();
 
-    //  creates the objects of the arrays using the input lengths
-    public void initStore(int numComputer, int numPrinters){
-        this.computers = new ArrayList<>(numComputer);
-        this.printers = new ArrayList<>(numPrinters);
-    }
+//    //  creates the objects of the arrays using the input lengths
+//    public void initStore(int numComputer, int numPrinters){
+//        this.computers = new ArrayList<>(numComputer);
+//        this.printers = new ArrayList<>(numPrinters);
+//    }
 
     // creating three computers and saving them in the array "computers"
     public void createComputers() {
         String name, description, manufacture;
         Scanner sel = new Scanner(System.in);
-        System.out.print("Enter Device Name:");
+        System.out.print("Enter Device (computer) Name:");
         name = sel.nextLine();
-        System.out.print("Enter Device Description:");
+        System.out.print("Enter Device (computer) Description:");
         description = sel.nextLine();
         System.out.print("Enter Computer Manufacture: ");
         manufacture = sel.next();
@@ -30,11 +31,11 @@ public class Store {
         String name, description;
         int ppm;
         Scanner sel = new Scanner(System.in);
-        System.out.print("Enter Device Name:");
+        System.out.print("Enter Device (printer) Name:");
         name = sel.nextLine();
-        System.out.print("Enter Device Description:");
+        System.out.print("Enter Device (printer) Description:");
         description = sel.nextLine();
-        System.out.print("Enter Computer ppm: ");
+        System.out.print("Enter Printer ppm: ");
         ppm = sel.nextInt();
         Printer aPrinter = new Printer(name, description, ppm);
         printers.add(aPrinter);
@@ -54,14 +55,28 @@ public class Store {
         }
     }
 
-    // calling all creation and printing methods
-    public void runBazar(){
-        initStore(3, 2);
-        createComputers();
-        createPrinters();
-        printComputers();
-        printPrinters();
+    public static int menuItem() {
+        Scanner sel = new Scanner(System.in);
+
+        System.out.println("1) New Computer");
+        System.out.println("2) New Printer");
+        System.out.println("3) List Computers");
+        System.out.println("4) List Printers");
+        System.out.println("5) Exit");
+        System.out.print("Select one:");
+        int choice = Integer.parseInt(sel.nextLine());
+        System.out.println("Your choice:"+choice);
+        return choice;
     }
+
+    // calling all creation and printing methods
+//    public void runBazar(){
+//        initStore(3, 2);
+//        createComputers();
+//        createPrinters();
+//        printComputers();
+//        printPrinters();
+//    }
 
 
 
--- src/Utils.java
+++ src/Utils.java
@@ -6,4 +6,4 @@ public class Utils {
         return (r.nextInt(high - low) + low);
     }
 }
-//
+
